b443aea93468d77fc6b5317c2dce8023aab4f3de
Arthur Wu <wuanch@gmail.com>
2015-10-01 21:40:53 +0800

    change big style

--- mqttclient.js
+++ mqttclient.js
@@ -17,22 +17,25 @@ var settings = {
 }
 
 var client = mqtt.connect('ws://acnlp.com:5000', settings);
+client.subscribe("presence", {qos: 1});
 
-for(var i=0;i<100;i++) {
-    client.publish("presence", 'hello offline msg'+i);
+for(var i=0;i<10000;i++) {
+
+    client.subscribe("presence", {qos: 1});
+    //client.publish("presence", 'hello offline msg'+i);
 }
 //var client  = mqtt.connect('ws://127.0.0.1:5000',settings);
 //client.publish("presence", 'hello offline msg1',{qos:1});
 //client.publish("presence", 'hello offline msg2',{qos:1});
 //client.publish("presence", 'hello offline msg3',{qos:1});
 //client.publish("presence", 'hello offline msg4',{qos:1});
-client.end();
+//client.end();
 
-//client.on('message', function (topic, message) {
-//    // message is Buffer
-//    console.log(message.toString());
-//    //client.end();
-//});
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    //client.end();
+});
 
 
 
